ce43e79f920033186ec8a303c38dec9c39b2c3b3
Thomas M rebicki <Tmrebicki@gmail.com>
2026-02-24 10:00:02 -0700

    Wrote the code for Prep1. Final and working.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Console.WriteLine("Hello Prep1 World!");
+        string fName;
+        string sName;
+
+
+        Console.WriteLine("What is your first name?: ");
+        fName = Console.ReadLine();
+    
+        Console.WriteLine("What is your last name?: ");
+        sName = Console.ReadLine();
+
+        Console.WriteLine("Your name is " + sName + ", " + fName + " " + sName);
     }
 }
