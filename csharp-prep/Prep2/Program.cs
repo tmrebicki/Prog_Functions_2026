40e796c584cb340db4575004afd77c8a96f3fbb0
Thomas M rebicki <Tmrebicki@gmail.com>
2026-02-25 10:03:56 -0700

    If statements prep

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int x;
+        string grade;
+        int GG;
+        string strch;
+        Console.WriteLine("What was your grade? (in percentage)");
+
+        string resp = Console.ReadLine();
+    
+        x = int.Parse(resp); 
+
+    
+
+
+        if (x >= 90)
+        {
+            
+            grade = "A";
+
+        }else if (x >= 80)
+        {
+            
+            grade = "B";
+
+        }else if (x >= 70)
+        {
+            
+            grade = "C";
+
+        }else if (x>= 60)
+        {
+
+            grade = "D";
+
+        }
+        else
+        {
+            
+            grade = "F";
+
+        }
+
+        Console.WriteLine("Your grade is:" + grade);
+
+
     }
 }
