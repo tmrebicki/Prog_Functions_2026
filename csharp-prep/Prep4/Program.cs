9305e410050d904f64b594277464b89f29baec66
Thomas M rebicki <Tmrebicki@gmail.com>
2026-02-26 10:50:20 -0700

    Lists core + strch

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,73 @@
 using System;
-
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
 class Program
 {
+
+  
+ 
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        int x = 0;
+        int end =0;
+        int total = 0;
+        int avg =0;
+        int larg =0;
+        int small =10^200;
+        List<int> numbers = new List<int>();
+
+        while (end == 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            Console.WriteLine("Enter a number:");
+            string resp = Console.ReadLine();
+    
+            x = int.Parse(resp); 
+
+            if (larg < x)
+            {
+                larg = x;
+            }
+           if (x > 0 && x < small)
+            {
+                small = x;
+            }
+
+             if (x == 0)
+            {
+            
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    total += numbers[i];
+                }
+
+                avg = total / numbers.Count;
+                numbers.Sort();
+
+                Console.WriteLine("Sum: " + total);
+                Console.WriteLine("average: " + avg);
+                Console.WriteLine("Largest number: " + larg);
+                Console.WriteLine("The smallest positive number: " + small);
+                Console.WriteLine("sorted list");
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                Console.WriteLine(numbers[i]);
+                }
+                end = 1;
+
+
+
+            }   
+            else
+            {
+                numbers.Add(x);
+            }
+        }
+
+    }   
+
+
 }
