6c92778fdda2af7ef2f25d789b9dfd0ef566bccb
Thomas M rebicki <Tmrebicki@gmail.com>
2026-02-26 11:26:34 -0700

    Functions core

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,62 @@
 using System;
 
 class Program
-{
+{   
+
+
+    static void Displaywelcome()
+    {
+        
+        Console.WriteLine("Welcome to the Program!");
+
+    }
+    static string Promptusername()
+    {
+        
+        Console.WriteLine("What is your name?: ");
+        string i = Console.ReadLine();
+        return i;
+    
+    }
+    static int Promptnumber()
+    {
+        Console.WriteLine("What is your favorite number");
+        string i = Console.ReadLine();
+        int x = int.Parse(i);
+        return x;
+    }
+    static int Sqrnumber(int l, string name)
+    {
+        int f = + l^2;
+        return f;
+
+    }
+    static int PromptBirthYear(string name)
+    {
+        Console.WriteLine(name + ", what is your year of birth?");
+
+        string i = Console.ReadLine();
+        int x = int.Parse(i);
+        x -= 2026;
+        return x*-1;
+
+    }
+
+    static void displayresults(string name, int num, int year )
+    {
+
+        Console.WriteLine(name + ",your number squared is: " + num);
+        Console.WriteLine(name + ",you will turn: " + year);
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Displaywelcome();
+        string name = Program.Promptusername();
+        int num = Program.Promptnumber();
+        int ye = Program.PromptBirthYear(name);
+        int sqr = Program.Sqrnumber(num, name);
+        displayresults(name, sqr, ye);
+
     }
 }
